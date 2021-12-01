274acea99a201f6d558698bc91ab0d00484d4f13
Dmytro Boiko <homen932@gmail.com>
2021-12-01 19:52:01 +0100

    Test

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Navigation from "./components/navbar/NavigationBar";
 import ParticleAnimation from "react-particle-animation";
 import Header from "./components/animationText/AnimationText";
 import AboutMe from "./components/about-me/About-me";
-import Project from "./components/project/Project";
-import Contact from "./components/contact/Contact";
+// import Project from "./components/project/Project";
+// import Contact from "./components/contact/Contact";
 
 function App() {
   return (
@@ -25,12 +25,8 @@ function App() {
           <Route path="/about-me">
             <AboutMe />
           </Route>
-          <Route path="/projects">
-            <Project />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
+          <Route path="/projects">{/* <Project /> */}</Route>
+          <Route path="/contact">{/* <Contact /> */}</Route>
         </Switch>
       </div>
     </Router>

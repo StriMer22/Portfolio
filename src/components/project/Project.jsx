4bffbc27be3160408efee26e5e386aca2d30d4af
Dmytro Boiko <homen932@gmail.com>
2021-12-03 23:37:36 +0100

    Added project page

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './project.css';
 import Footer from '../footer/Footer';
+import { ReactComponent as Gear} from "../../assets/gear.svg";
 
 export default function project() {
     return (
-        <div>
+        <div className="container_project">
+            <div className="container_gear">
+            <Gear/>
+            <h1>Cooming soon...</h1>
+            </div>
+            
             <Footer />
         </div>
     )
 }
+
+
